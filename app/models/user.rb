127563a3041279af74bb46ba54eafa87a90167a9
okamu0414 <s.okamuu@icloud.com>
2020-12-30 16:16:22 +0900

    Add columns name and profile to UserModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
+
+  #nameは必ず入力されていないとだめ
+  validates :name, presence: true
+  #profileは200文字以内
+  validates :profile, length: { maximum: 200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:show] 
   root 'static_pages#home'
-  get 'profile', to: 'users#show'
-  
   
 end
