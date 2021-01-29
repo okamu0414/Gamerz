f0b93ed31d511e46cfac0327ac54eebca2f5ce8a
okamu0414 <s.okamuu@icloud.com>
2021-01-30 01:39:01 +0900

    Add Recruitiment Validates

--- app/models/recruitiment.rb
+++ app/models/recruitiment.rb
@@ -1,3 +1,9 @@
 class Recruitiment < ApplicationRecord
   belongs_to :users
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :play_style, presence: true, lenth: { maximum: 100 }
+  validates :used_vc, presence: true, length: { maximum: 140 }
+  validates :one_word, presence: true, length: { maximum: 140 }
+  
 end
